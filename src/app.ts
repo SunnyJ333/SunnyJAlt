5d6f81a88de735e0c06f26282d1143095a7aa9ef
SunnyJ <jinseon333@gmail.com>
2022-02-11 14:57:14 +0900

    tree test

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ export default class HelloWorld {
 				// Parent the glTF model to the text actor, so the transform is relative to the text
 				transform: {
 					local: {
-						position: { x: 0, y: -1, z: 0 },
+						position: { x: 0, y: 0, z: 0 },
 						scale: { x: 0.3, y: 0.3, z: 0.3 }
 					}
 				}
