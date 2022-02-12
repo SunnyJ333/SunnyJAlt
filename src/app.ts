769c458d1ce1653c2be4651033d78208ff7f540c
SunnyJ <jinseon333@gmail.com>
2022-02-12 20:05:34 +0900

    attach sound test

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import * as MRE from "@microsoft/mixed-reality-extension-sdk";
 
 /**
  * The main class of this app. All the logic goes here.
@@ -17,6 +17,8 @@ export default class HelloWorld {
 	private soundActor?: MRE.Actor = undefined;
 	private soundOronC?: MRE.Sound = undefined;
 
+	private attachedHats = new Map<MRE.Guid, MRE.Actor>();
+
 	constructor(private context: MRE.Context) {
 		this.context.onStarted(() => this.started());
 	}
@@ -27,50 +29,49 @@ export default class HelloWorld {
 	private async started() {
 		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
 		this.assets = new MRE.AssetContainer(this.context);
-		
-		
 
-		this.soundOronC = this.assets.createSound('soundOronaminC',
-		{uri: 'https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1934786944458294000/ogg_OronaminC.ogg'});
+		this.soundOronC = this.assets.createSound("soundOronaminC", {
+			uri: "https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1934786944458294000/ogg_OronaminC.ogg",
+		});
 
 		// spawn a copy of the glTF model
 		this.cube = MRE.Actor.CreateFromLibrary(this.context, {
 			// Also apply the following generic actor properties.
 			actor: {
-				name: 'Altspace Cube',
+				name: "Altspace Cube",
 				// Parent the glTF model to the text actor, so the transform is relative to the text
 				transform: {
 					local: {
 						position: { x: 0, y: -0.5, z: 0 },
-						scale: { x: 0.3, y: 0.3, z: 0.3 }
-					}
-				}
+						scale: { x: 0.3, y: 0.3, z: 0.3 },
+					},
+				},
 			},
-			resourceId: 'artifact:1931375002129531487'
+			resourceId: "artifact:1931375002129531487",
 		});
 
 		this.cube.setCollider(MRE.ColliderType.Auto, false);
-
-		this.soundActor = MRE.Actor.CreateEmpty(this.context, {
-			actor: {
-				name: 'Sound Actor',
-				parentId: this.cube.id
-
-			}
-		});
 
 		// Create some animations on the cube.
 		const flipAnimData = this.assets.createAnimationData(
 			// the animation name
 			"DoAFlip",
-			{ tracks: [{
-				// applies to the rotation of an unknown actor we'll refer to as "target"
-				target: MRE.ActorPath("target").transform.local.rotation,
-				// do a spin around the X axis over the course of one second
-				keyframes: this.generateSpinKeyframes( 13.0, new MRE.Vector3(0.0, 1.0, 0.0)),
-				// and do it smoothly
-				easing: MRE.AnimationEaseCurves.Linear
-			}]}
+			{
+				tracks: [
+					{
+						// applies to the rotation of an unknown actor we'll refer to as "target"
+						target: MRE.ActorPath("target").transform.local
+							.rotation,
+						// do a spin around the X axis over the course of one second
+						keyframes: this.generateSpinKeyframes(
+							13.0,
+							new MRE.Vector3(0.0, 1.0, 0.0)
+						),
+						// and do it smoothly
+						easing: MRE.AnimationEaseCurves.Linear,
+					},
+				],
+			}
 		);
 		// apply the animation to our cube
 		const flipAnim = await flipAnimData.bind({ target: this.cube });
@@ -80,37 +81,65 @@ export default class HelloWorld {
 		const buttonBehavior = this.cube.setBehavior(MRE.ButtonBehavior);
 
 		// Trigger the grow/shrink animations on hover.
-		buttonBehavior.onHover('enter', () => {
+		buttonBehavior.onHover("enter", () => {
 			// use the convenience function "AnimateTo" instead of creating the animation data in advance
 			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
+				destination: {
+					transform: {
+						local: { scale: { x: 0.35, y: 0.35, z: 0.35 } },
+					},
+				},
 				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
+				easing: MRE.AnimationEaseCurves.EaseOutSine,
 			});
 		});
-		buttonBehavior.onHover('exit', () => {
+		buttonBehavior.onHover("exit", () => {
 			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.3, y: 0.3, z: 0.3 } } } },
+				destination: {
+					transform: { local: { scale: { x: 0.3, y: 0.3, z: 0.3 } } },
+				},
 				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
+				easing: MRE.AnimationEaseCurves.EaseOutSine,
 			});
 		});
 
 		// When clicked, do a 360 sideways.
-		buttonBehavior.onClick(_ => {
+		buttonBehavior.onClick((user) => {
 			flipAnim.play();
 
-			this.soundActor.startSound(this.soundOronC.id,{
-				doppler: 0.0,
-				looping: false,
-				paused: false,
-				pitch: 0,
-				rolloffStartDistance: 0.1,
-				spread: 0.0,
-				time: 0,
-				volume: 1.0
-			});
-			
+			this.wearFakeHat(user.id);
+		});
+	}
+
+	private wearFakeHat(userId: MRE.Guid) {
+		if (this.attachedHats.has(userId))
+			this.attachedHats.get(userId).destroy();
+		this.attachedHats.delete(userId);
+
+		const attachPoint = <MRE.AttachPoint>{ attachPoint: "head" };
+
+		this.soundActor = new MRE.Actor.CreateEmpty(this.context, {
+			actor: {
+				name: "Sound Actor",
+
+				attachment: {
+					attachPoint: attachPoint,
+					userId,
+				},
+			},
+		});
+
+		this.attachedHats.set(userId, this.soundActor);
+
+		this.soundActor.startSound(this.soundOronC.id, {
+			doppler: 0.0,
+			looping: false,
+			paused: false,
+			pitch: 0,
+			rolloffStartDistance: 0.1,
+			spread: 0.0,
+			time: 0,
+			volume: 1.0,
 		});
 	}
 
@@ -119,22 +148,31 @@ export default class HelloWorld {
 	 * @param duration The length of time in seconds it takes to complete a full revolution.
 	 * @param axis The axis of rotation in local space.
 	 */
-	private generateSpinKeyframes(duration: number, axis: MRE.Vector3): Array<MRE.Keyframe<MRE.Quaternion>> {
-		return [{
-			time: 0 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 0)
-		}, {
-			time: 0.25 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, Math.PI / 2)
-		}, {
-			time: 0.5 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, Math.PI)
-		}, {
-			time: 0.75 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 3 * Math.PI / 2)
-		}, {
-			time: 1 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 2 * Math.PI)
-		}];
+	private generateSpinKeyframes(
+		duration: number,
+		axis: MRE.Vector3
+	): Array<MRE.Keyframe<MRE.Quaternion>> {
+		return [
+			{
+				time: 0 * duration,
+				value: MRE.Quaternion.RotationAxis(axis, 0),
+			},
+			{
+				time: 0.25 * duration,
+				value: MRE.Quaternion.RotationAxis(axis, Math.PI / 2),
+			},
+			{
+				time: 0.5 * duration,
+				value: MRE.Quaternion.RotationAxis(axis, Math.PI),
+			},
+			{
+				time: 0.75 * duration,
+				value: MRE.Quaternion.RotationAxis(axis, (3 * Math.PI) / 2),
+			},
+			{
+				time: 1 * duration,
+				value: MRE.Quaternion.RotationAxis(axis, 2 * Math.PI),
+			},
+		];
 	}
 }
