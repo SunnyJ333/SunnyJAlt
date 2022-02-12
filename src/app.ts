3e85139c18fe22b0742126f5fe2086d43404fe9b
SunnyJ <jinseon333@gmail.com>
2022-02-13 01:34:05 +0900

    test-userjoined

--- src/app.ts
+++ src/app.ts
@@ -19,17 +19,42 @@ export default class HelloWorld {
 
 	constructor(private context: MRE.Context) {
 		this.context.onStarted(() => this.started());
-		this.context.onUserJoined(user => this.onUserJoined(user));
+		this.context.userJoined(user => this.userJoined(user));
 	}
 
 	/**
 	 * Once the context is "started", initialize the app.
 	 */
-	private async started() {
+	//private async started() {}
+
+	private async userJoined(user: MRE.User) {
 		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
 		this.assets = new MRE.AssetContainer(this.context);
 
-		
+		// spawn a copy of the glTF model
+		this.cube = MRE.Actor.CreateFromLibrary(this.context, {
+			// Also apply the following generic actor properties.
+			actor: {
+				name: "Altspace Cube",
+				// Parent the glTF model to the text actor, so the transform is relative to the text
+				transform: {
+					local: {
+						position: { x: 0, y: -0.5, z: 0 },
+						scale: { x: 0.3, y: 0.3, z: 0.3 },
+					},
+				},
+			},
+			resourceId: "artifact:1931375002129531487",
+		});
+
+		this.cube.setCollider(MRE.ColliderType.Auto, false);
+
+		const soundActor = MRE.Actor.CreateEmpty(this.context, {
+			actor: {
+				name: "Sound Actor",
+				parentId: this.cube.id,
+			},
+		});
 
 		this.soundOronC = this.assets.createSound("soundOronaminC", {
 			uri: "https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1934786944458294000/ogg_OronaminC.ogg",
@@ -98,35 +123,6 @@ export default class HelloWorld {
 				volume: 0.9,
 			});
 		});
-	}
-
-	private userJoined(user: MRE.User) {
-
-		// spawn a copy of the glTF model
-		this.cube = MRE.Actor.CreateFromLibrary(this.context, {
-			// Also apply the following generic actor properties.
-			actor: {
-				name: "Altspace Cube",
-				// Parent the glTF model to the text actor, so the transform is relative to the text
-				transform: {
-					local: {
-						position: { x: 0, y: -0.5, z: 0 },
-						scale: { x: 0.3, y: 0.3, z: 0.3 },
-					},
-				},
-			},
-			resourceId: "artifact:1931375002129531487",
-		});
-
-		this.cube.setCollider(MRE.ColliderType.Auto, false);
-
-		this.soundActor = MRE.Actor.CreateEmpty(this.context, {
-			actor: {
-				name: "Sound Actor",
-				parentId: this.cube.id,
-			},
-		});
-
 	}
 
 	/**
